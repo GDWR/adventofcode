23e9afdb6df72c733db4ae54f29a6fde46594d8e
GDWR <gregory.dwr@gmail.com>
2023-12-02 14:37:20 +0000

    2023 day2 part 1

---
+++ 2023/day2/main.rs
@@ -0,0 +1,64 @@
+const INPUT: &str = include_str!("input.txt");
+const RED_MAX: usize = 12;
+const GREEN_MAX: usize = 13;
+const BLUE_MAX: usize = 14;
+
+fn main() {
+    let games = parse_input(INPUT);
+
+    let part_one: usize = games.iter()
+        .map(|g| g.rounds.iter().any(|r| r.red > RED_MAX || r.green > GREEN_MAX || r.blue > BLUE_MAX))
+        .enumerate()
+        .map(|(i, v)| (1+i) * !v as usize)
+        .sum();
+
+    println!("Part one: {}", part_one);
+}
+
+
+fn parse_input(input: &str) -> Vec<Game> {
+    let mut games: Vec<Game> = vec![];
+
+    for (i_game, game_raw) in input.lines().enumerate() {
+        let Some((_, r)) = game_raw.split_once(":") else {
+            panic!("Invalid input");
+        };
+
+        let mut game = Game::default();
+
+        for (i_round, round_raw) in r.split(";").enumerate() {
+            let mut round = Round::default();
+
+            for cube in round_raw.split(",") {
+                let Some((count, colour)) = cube.trim().split_once(" ") else {
+                    panic!("Invalid input");
+                };
+
+                match colour {
+                    "red" => round.red = count.parse::<usize>().unwrap(),
+                    "green" => round.green = count.parse::<usize>().unwrap(),
+                    "blue" => round.blue = count.parse::<usize>().unwrap(),
+                    _ => panic!("Invalid input"),
+                }
+            }
+
+            game.rounds.push(round);
+        }
+
+        games.push(game);
+    }
+
+    return games;
+}
+
+#[derive(Debug, Default)]
+struct Game {
+    rounds: Vec<Round>,
+}
+
+#[derive(Debug, Default)]
+struct Round {
+    pub red: usize,
+    green: usize,
+    blue: usize,
+}
