a3834c0dccc61a5049d9fb95093da68820d32e3e
GDWR <gregory.dwr@gmail.com>
2023-12-03 13:59:11 +0000

    2023 day3 part two

--- 2023/day3/main.kts
+++ 2023/day3/main.kts
@@ -1,51 +1,94 @@
 import java.io.File
 
-val input = File("input.txt").readLines()
+val input = File("input.txt").readText()
+val engineSchematic = EngineSchematic.parse(input)
 
-// Bool mask, for each symbol and its surrounding cells
-val mask = Array(input.size) {
-    Array(input.first().length) { false }
-}.also {
-    input.forEachIndexed { i, line ->
-        line.forEachIndexed { j, char ->
-            if (char != '.' && !char.isDigit()) {
-                (-1..1).forEach { iOff ->
-                    (-1..1).forEach { jOff ->
-                        // this could be out of bounds, but puzzle author was nice
-                        it[i + iOff][j + jOff] = true
+val partOne = engineSchematic.parts
+        .filter { engineSchematic.getSymbolsInRange(it.spanIndex, it.startIndex.y).any() }
+        .sumOf { it.number }
+
+println("Part one: $partOne")
+
+val partTwo = engineSchematic.symbols
+        .filter { it.char == '*' }
+        .map { engineSchematic.getPartsAroundIndex(it.index) }
+        .filter { it.size == 2 }
+        .sumOf { it[0].number * it[1].number }
+
+println("Part two: $partTwo")
+
+
+data class Index(val x: Int, val y: Int)
+
+// Technically these are not "Part"s as defined in the problem, but I wanted to capture
+//    all the numbers in a single data class.
+data class Part(val number: Int, val startIndex: Index) {
+    val endIndex: Index
+        get() = Index(startIndex.x + number.toString().length - 1, startIndex.y)
+
+    val rowIndex: Int
+        get() = startIndex.y
+
+    // Span over y, as a part cannot span over rows
+    val spanIndex: IntRange
+        get() = startIndex.x..endIndex.x
+}
+
+data class Symbol(val char: Char, val index: Index)
+
+data class EngineSchematic(val parts: List<Part>, val symbols: List<Symbol>) {
+    companion object {
+        fun parse(input: String): EngineSchematic {
+            val symbols = mutableListOf<Symbol>()
+            val parts = mutableListOf<Part>()
+
+            for ((y, line) in input.lines().withIndex()) {
+                var numBuffer = ""
+
+                for ((x, char) in line.withIndex()) {
+                    if (char == '.') {
+                        if (numBuffer.isNotEmpty()) {
+                            val newPart = Part(numBuffer.toInt(), Index(x - numBuffer.length, y))
+                            parts.add(newPart)
+                            numBuffer = ""
+                        }
+
+                        continue
+                    } else if (char.isDigit()) {
+                        // If we used a while loop, we could forward the parse until we capture
+                        //   the entire number. For now, we utilize scoped variables for this state.
+                        numBuffer += char
+                    } else {
+                        val newSymbol = Symbol(char, Index(x, y))
+                        symbols.add(newSymbol)
+
+                        if (numBuffer.isNotEmpty()) {
+                            val newPart = Part(numBuffer.toInt(), Index(x - numBuffer.length, y))
+                            parts.add(newPart)
+                            numBuffer = ""
+                        }
                     }
                 }
-            }
-        }
-    }
-}
 
-
-var partOne = mutableListOf<Int>()
-
-for ((i, row) in input.withIndex()) {
-    var numBuffer = ""
-    var inRangeOfSymbol = false
-
-    for ((j, char) in row.withIndex()) {
-        if (char.isDigit()) {
-            if (!inRangeOfSymbol && mask[i][j]) {
-                inRangeOfSymbol = true
+                if (numBuffer.isNotEmpty()) {
+                    val newPart = Part(numBuffer.toInt(), Index(line.length - numBuffer.length, y))
+                    parts.add(newPart)
+                }
             }
 
-            numBuffer += char
-        } else {
-            if (inRangeOfSymbol && numBuffer.isNotEmpty()) {
-                partOne.add(numBuffer.toInt())
-            }
-            numBuffer = ""
-            inRangeOfSymbol = false
+            return EngineSchematic(parts = parts, symbols = symbols)
         }
     }
 
-    if (inRangeOfSymbol && numBuffer.isNotEmpty()) {
-        partOne.add(numBuffer.toInt())
+    fun getSymbolsInRange(range: IntRange, y: Int): List<Symbol> {
+        return symbols
+                .filter { symbol -> symbol.index.y in y - 1..y + 1 }
+                .filter { symbol -> symbol.index.x in range.first - 1..range.last + 1 }
+    }
+
+    fun getPartsAroundIndex(index: Index): List<Part> {
+        return parts
+            .filter { part -> part.startIndex.y in index.y - 1..index.y + 1 }
+            .filter { part -> index.x in part.startIndex.x-1..part.endIndex.x+1 }
     }
 }
-
-println("Part one: ${partOne.sum()}")
